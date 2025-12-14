27841c5c98b25c3491956aa0dad5cec4f8fc3d53
KAZI RAJWAN SULTAN <111181255+Rajwan-Sultan@users.noreply.github.com>
2025-12-14 16:47:33 +0600

    Update model version from 2.0 to 2.5

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ async def parse_with_llm_audio(audio_bytes: bytes, mime_type: str, segment_numbe
     )
 
     response = await gemini_client.aio.models.generate_content(
-        model="gemini-2.0-flash-lite",
+        model="gemini-2.5-flash-lite",
         # model="gemini-2.0-flash-exp",
         contents=parts,
         config=generation_config
